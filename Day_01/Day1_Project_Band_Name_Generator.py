b0d837e800552073bf32869f3f354efe0c06d93f
chiachiamaru <chiakinishihara.jp@gmail.com>
2023-05-16 15:39:25 +0200

    Organize

--- Day_01/Day1_Project_Band_Name_Generator.py
+++ Day_01/Day1_Project_Band_Name_Generator.py
@@ -3,5 +3,5 @@ print("Hello :) Welcome to the Band Name Generator.")
 city_name = input("Which city did you grow up in?\n")
 
 pet_name = input("Could you tell me the name of your pet?\n")
-# finalprint
+
 print ("Here is your band name: " + city_name +" "+ pet_name)
