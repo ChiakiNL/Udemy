14b4618b0e674ea363863e633338eb8c9ba07935
chiachiamaru <chiakinishihara.jp@gmail.com>
2023-06-12 13:54:25 +0200

    Create Day4_Heads_or_Tails.py

---
+++ Day_04/Day4_Heads_or_Tails.py
@@ -0,0 +1,14 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+
+import random
+
+randomInteger = random.randint(0, 1)
+
+if randomInteger == 0: 
+    print("Tails")
+
+else:
+    print("Heads")
