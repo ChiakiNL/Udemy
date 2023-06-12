68ea2d906ba2a488a3d6eeb6a596a04e50b0c12f
chiachiamaru <chiakinishihara.jp@gmail.com>
2023-06-12 16:13:02 +0200

    Organize

--- Day_04/Day4_Banker_Roulette.py
+++ Day_04/Day4_Banker_Roulette.py
@@ -4,7 +4,10 @@ import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
-length = len(names) - 1
-random_number = random.randint(0, length)
+person_will_pay = random.choice(names)
+print (f"{person_will_pay} is going to buy the meal today!")
 
-print(f"{names[random_number]} is going to buy the meal today!")
+# length = len(names) - 1
+# random_number = random.randint(0, length)
+
+# print(f"{names[random_number]} is going to buy the meal today!")
