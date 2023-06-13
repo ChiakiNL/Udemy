407c1551351b228e2834931601916b22d87aac93
chiachiamaru <chiakinishihara.jp@gmail.com>
2023-06-13 15:23:22 +0200

    Organize

---
+++ Day_05/Day5_Adding_Even_Numbers.py
@@ -0,0 +1,25 @@
+#Write your code below this row 👇
+
+total = 0
+
+for number in range(0, 101, 2):
+    total += number
+
+print(total)
+
+###ANSWER1###
+
+# total = 0
+# for number in range(2, 101, 2):
+#     total += number
+
+# print(total)
+
+###ANSWER2###
+
+# total = 0
+# for number in range(1, 101):
+#     if number % 2 == 0:
+#         total += number
+
+# print(total)
