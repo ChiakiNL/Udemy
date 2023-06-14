ffc44f30fbddccb46576ff742508960ef2f5741d
chiachiamaru <chiakinishihara.jp@gmail.com>
2023-06-14 14:38:49 +0200

    Organize

---
+++ Day_06/Day6_Reeborgs_World_WhileLoop.py
@@ -0,0 +1,25 @@
+#Reeborg's Hurdles Race
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():  
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    jump()
+
+### Another way ###
+#Not equal to: !=
+#while at_goal() != True:
+#    jump()
