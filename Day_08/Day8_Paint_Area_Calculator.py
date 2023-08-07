db3ba53663c2fe805e124b7967b15b221a77ac4b
chiachiamaru <chiakinishihara.jp@gmail.com>
2023-08-07 20:21:58 +0200

    Updated

--- Day_08/Day8_Paint_Area_Calculator.py
+++ Day_08/Day8_Paint_Area_Calculator.py
@@ -1,10 +1,11 @@
-height = int(input("Height of wall: "))
-width = int(input("Width of wall: "))
-cover = 5
+import math
+def paint_calc(height, width, cover):
+    result = (height*width)/cover
+    rounded_result = math.ceil(result)
+    print(f"You'll need {rounded_result} cans of paint.")
 
-import math 
-number_of_cans = (height * width) / cover
-
-rounded_cans = math.ceil(number_of_cans)
-
-print(f"You'll need {rounded_cans} cans of paint.")
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
